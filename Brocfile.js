b34155034a9d505e46b10e1f9b1b78a3dc8cf06a
mlunoe <michael.lunoe@gmail.com>
2014-10-20 15:28:25 -0700

    removing semicolons

--- Brocfile.js
+++ Brocfile.js
@@ -1,69 +1,69 @@
-var filterCoffeeScript = require('broccoli-coffee');
-var filterTemplates = require('broccoli-template');
-var uglifyJavaScript = require('broccoli-uglify-js');
-var compileES6 = require('broccoli-es6-concatenator');
-var compileSass = require('broccoli-sass');
-var pickFiles = require('broccoli-static-compiler');
-var mergeTrees = require('broccoli-merge-trees');
-var findBowerTrees = require('broccoli-bower');
-var env = require('broccoli-env').getEnv();
+var filterCoffeeScript = require('broccoli-coffee')
+var filterTemplates = require('broccoli-template')
+var uglifyJavaScript = require('broccoli-uglify-js')
+var compileES6 = require('broccoli-es6-concatenator')
+var compileSass = require('broccoli-sass')
+var pickFiles = require('broccoli-static-compiler')
+var mergeTrees = require('broccoli-merge-trees')
+var findBowerTrees = require('broccoli-bower')
+var env = require('broccoli-env').getEnv()
 
 function preprocess (tree) {
   // filter .hbs and .handlebar files, use the Ember handlebar compiler
   tree = filterTemplates(tree, {
     extensions: ['hbs', 'handlebars'],
     compileFunction: 'Ember.Handlebars.compile'
-  });
+  })
 
   // compile coffee script, bare: no top-level function wrapper
   tree = filterCoffeeScript(tree, {
     bare: true
-  });
-  return tree;
+  })
+  return tree
 }
 
 // create tree for files in the app folder
-var app = 'app';
+var app = 'app'
 app = pickFiles(app, {
   srcDir: '/',
   destDir: 'appkit' // move under appkit namespace
-});
-app = preprocess(app);
+})
+app = preprocess(app)
 
 // create tree for files in the styles folder
-var styles = 'styles';
+var styles = 'styles'
 styles = pickFiles(styles, {
   srcDir: '/',
   destDir: 'appkit' // move under appkit namespace
-});
-styles = preprocess(styles);
+})
+styles = preprocess(styles)
 
 // create tree for files in the test folder
-var tests = 'tests';
+var tests = 'tests'
 tests = pickFiles(tests, {
   srcDir: '/',
   destDir: 'appkit/tests' // move under appkit namespace
-});
-tests = preprocess(tests);
+})
+tests = preprocess(tests)
 
 // create tree for vendor folder (no filters needed here)
-var vendor = 'vendor';
+var vendor = 'vendor'
 
 // include app, styles and vendor trees
-var sourceTrees = [app, styles, vendor];
+var sourceTrees = [app, styles, vendor]
 
 // include tests if in production
 if (env !== 'production') {
-  sourceTrees.push(tests);
+  sourceTrees.push(tests)
 }
 
 // Add bower dependencies
 // findBowerTrees uses heuristics to pick the lib directory and/or main files,
 // and returns an array of trees for each bower package found.
-sourceTrees = sourceTrees.concat(findBowerTrees());
+sourceTrees = sourceTrees.concat(findBowerTrees())
 
 // merge array into tree
-var appAndDependencies = new mergeTrees(sourceTrees, { overwrite: true });
+var appAndDependencies = new mergeTrees(sourceTrees, { overwrite: true })
 
 // Transpile ES6 modules and concatenate them,
 // recursively including modules referenced by import statements.
@@ -85,22 +85,22 @@ var appJs = compileES6(appAndDependencies, {
   ],
   wrapInEval: env !== 'production',
   outputFile: '/assets/app.js'
-});
+})
 
 // compile sass
-var appCss = compileSass(sourceTrees, 'appkit/app.scss', 'assets/app.css');
+var appCss = compileSass(sourceTrees, 'appkit/app.scss', 'assets/app.css')
 
 if (env === 'production') {
   // minify js
   appJs = uglifyJavaScript(appJs, {
     // mangle: false,
     // compress: false
-  });
+  })
 }
 
 // create tree for public folder (no filters needed here)
-var publicFiles = 'public';
+var publicFiles = 'public'
 
 // merge js, css and public file trees, and export the
-module.exports = mergeTrees([appJs, appCss, publicFiles]);
+module.exports = mergeTrees([appJs, appCss, publicFiles])
 
